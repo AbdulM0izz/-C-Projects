e96c987cc5579b4bfc31f20499ad013002934b95
Abdul Moiz <abdulmoiz95972@gmail.com>
2024-11-11 01:06:27 +0500

    Atm Completed

--- ATM MACHINE PROJECT/ATM.cpp
+++ ATM MACHINE PROJECT/ATM.cpp
@@ -1,48 +1,79 @@
 #include <iostream>
 using namespace std;
-class ATM{
-    private:
-    int balance ;
-    public:
-    ATM( int bal){
-         balance =bal;
-    }
-    int getBalance(){
+
+class ATM {
+private:
+    int balance;
+
+public:
+    ATM(int bal) : balance(bal) {}
+
+    int getBalance() const {
         return balance;
     }
-    int withdraw( int amount){
-        if (amount > balance){
+
+    bool withdraw(int amount) {
+        if (amount > balance) {
             return false;
         }
-        balance = balance - amount;
+        balance -= amount;
         return true;
-
     }
-    void deposit (int amount ){
-        balance = balance - amount;
+
+    void deposit(int amount) {
+        balance += amount;
     }
 };
 
-int main(){
-    cout<<"\tA T M    M A C H I N E:   ";
-    cout<<"\tPROJECT BY ABDUL MOIZ F24BSE010 "<<endl;
-    ATM atm(1000);
-    int choice;
-    cout<<"1. View Balance: "<<endl;
-    cout<<"2. Cash withdraw "<<endl;
-    cout<<"3. Cash deposit "<<endl;
-    cout<<"4. Exit"<<endl;
-    cout<<"Enter your choice : ";
-    cin>>choice;
-    switch(choice){
-        case 1:
-        cout<<"Your balance is :"<<atm.getBalance();
-        break;
-    }
+int main() {
    
+    ATM atm(1000);
+    int choice, amount;
+    char repeat;
     
+    	 cout << "\tA T M    M A C H I N E:";
+    cout << "\tPROJECT BY ABDUL MOIZ F24BSE010\n";
 
+    do {
+    	
+        cout << "1. View Balance:\n";
+        cout << "2. Cash Withdrawal\n";
+        cout << "3. Cash Deposit\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
 
+        switch (choice) {
+            case 1:
+                cout << "Your balance is: " << atm.getBalance() << endl;
+                break;
+            case 2:
+                cout << "Your available balance: " << atm.getBalance() << endl;
+                cout << "Enter the amount to withdraw: ";
+                cin >> amount;
+                if (atm.withdraw(amount)) {
+                    cout << "Withdrawal successful.\n";
+                } else {
+                    cout << "Insufficient balance.\n";
+                }
+                break;
+            case 3:
+                cout << "Enter amount to deposit: ";
+                cin >> amount;
+                atm.deposit(amount);
+                cout << "Deposit successful.\n";
+                break;
+            case 4:
+                cout << "Thanks for using Moiz's ATM.\n";
+                break;
+            default:
+                cout << "Invalid choice.\n";
+                break;
+        }
+
+        cout << "\nDo you want to try another transaction? [YES / NO]: ";
+        cin >> repeat;
+    } while (repeat == 'y' || repeat == 'Y');
 
     return 0;
 }
